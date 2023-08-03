76e48f4120634a3b121b7643ec9f17569c553558
Vatsal Rajgor <rv20197@gmail.com>
2023-08-03 19:29:06 +0530

    Api URL Bug Fix

--- lib/actions.ts
+++ lib/actions.ts
@@ -5,7 +5,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const apiUrl = isProduction
 	? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ''
-	: 'http://localhost:4000/graphql';
+	: 'http://127.0.0.1:4000/graphql';
 
 const apiKey = isProduction
 	? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ''
@@ -15,9 +15,10 @@ const serverUrl = isProduction
 	? process.env.NEXT_PUBLIC_SERVER_URL
 	: 'http://localhost:3000';
 
+const client = new GraphQLClient(apiUrl);
+
 const makeGraphQLRequest = async (query: string, variables = {}) => {
 	try {
-		const client = new GraphQLClient(apiUrl);
 		return await client.request(query, variables);
 	} catch (error) {
 		throw error;
@@ -25,6 +26,8 @@ const makeGraphQLRequest = async (query: string, variables = {}) => {
 };
 
 export const getUser = (email: string) => {
+	client.setHeader('x-api-key', apiKey);
+	client.setHeader('Authorization', `Bearer ${apiKey}`);
 	return makeGraphQLRequest(getUserQuery, { email });
 };
 
@@ -32,5 +35,7 @@ export const createUser = (name: string, email: string, avatarUrl: string) => {
 	const queryVariables = {
 		input: { name, email, avatarUrl }
 	};
+	client.setHeader('x-api-key', apiKey);
+	client.setHeader('Authorization', `Bearer ${apiKey}`);
 	return makeGraphQLRequest(createUserQuery, queryVariables);
 };
