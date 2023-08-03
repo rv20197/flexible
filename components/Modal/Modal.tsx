03ba51bc2e00b1677c7c6389ac59995069fa7a02
Vatsal Rajgor <rv20197@gmail.com>
2023-08-03 19:56:31 +0530

    Overlay Modal Added

---
+++ components/Modal/Modal.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useCallback, useRef, ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+
+const Modal = ({ children }: { children: ReactNode }) => {
+	const overlay = useRef<HTMLDivElement>(null);
+	const wrapper = useRef<HTMLDivElement>(null);
+
+	const router = useRouter();
+
+	const onClose = useCallback(() => {
+		router.push('/');
+	}, [router]);
+
+	const handleClick = useCallback((e: React.MouseEvent) => {
+		if ((e.target === overlay.current) && onClose) {
+			onClose()
+		}
+	}, [onClose, overlay]);
+
+	return (
+		<div ref={overlay} className='modal' onClick={handleClick}>
+			<button
+				type='button'
+				onClick={onClose}
+				className='absolute top-4 right-8'>
+				<Image src='close.svg' width={17} height={17} alt='close' />
+			</button>
+			<div ref={wrapper} className='modal_wrapper'>
+				{children}
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
