9df527cf55210a9660c0e9bc283e383165500745
Vatsal Rajgor <rv20197@gmail.com>
2023-08-05 06:52:00 +0530

    Id Issue Fixed

--- components/ProfileMenu/ProfileMenu.tsx
+++ components/ProfileMenu/ProfileMenu.tsx
@@ -14,7 +14,7 @@ const ProfileMenu = ({ session }: { session: SessionInterface }) => {
     return (
         <div className="flexCenter z-10 flex-col relative">
             <Menu as="div">
-                <Menu.Button className="flexCenter" onMouseEnter={() => setOpenModal(true)} >
+                <Menu.Button className="flexCenter" onMouseEnter={() => setOpenModal(true)} id='profile-button'>
                     {session?.user?.image && (
                         <Image
                             src={session.user.image}
