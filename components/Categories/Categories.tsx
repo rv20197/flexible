4fc72df8e2cd27416c7f3dbdab1d23cad9a65e19
Vatsal Rajgor <rv20197@gmail.com>
2023-08-06 08:55:17 +0530

    Category component done

---
+++ components/Categories/Categories.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { categoryFilters } from '../../constants';
+
+type Props = {};
+
+const Categories = (props: Props) => {
+	const router = useRouter();
+	const pathName = usePathname();
+	const searchParams = useSearchParams();
+
+	const category = searchParams.get('category');
+
+	const handleClick = (category: string) => {
+		router.push(`${pathName}?category=${category}`);
+	};
+
+	return (
+		<div className='flexBetween w-full gap-5 flex-wrap'>
+			<ul className='flex gap-2 overflow-auto'>
+				{categoryFilters.map((cat, index) => (
+					<button
+						key={index}
+						type='button'
+						className={`${
+							category === cat
+								? 'bg-light-white-300 font-medium'
+								: 'font-normal'
+						} px-4 py-3 rounded-lg capitalize whitespace-nowrap`}
+						onClick={() => handleClick(cat)}>
+						{cat}
+					</button>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+export default Categories;
