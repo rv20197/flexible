688717de217c38b84ba3323226c4a393d5ac14e0
Vatsal Rajgor <rv20197@gmail.com>
2023-08-16 08:37:22 +0530

    Removed reactProductionProfiling Flag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	reactProductionProfiling: true,
 	images: {
 		domains: ['lh3.googleusercontent.com', 'res.cloudinary.com']
 	},
